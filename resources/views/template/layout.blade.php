237a278aeb5b21e7eded75dbe39b21feef79c821
Muhammad Imam <saitamasensei2005@gmail.com>
2023-01-13 06:59:14 +0700

    melengkapi fitur cart

--- resources/views/template/layout.blade.php
+++ resources/views/template/layout.blade.php
@@ -20,110 +20,121 @@
 
     <style>
         ul.nav a:hover { color: rgb(245, 0, 0) !important; }
+
+        .bungkus1 {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            min-height: 100vh;
+        }
+
     </style>
 </head>
 
 <body>
 
-
-    <!-- Header -->
-    <nav class="navbar navbar-expand-lg navbar-light shadow">
-        <div class="container d-flex justify-content-between align-items-center">
-
-            <a class="navbar-brand text-danger logo h1 align-self-center" href="{{ url('/index') }}">
-                Roosen
-            </a>
-
-            <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse"
-                data-bs-target="#templatemo_main_nav" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="align-self-center collapse navbar-collapse flex-fill d-lg-flex justify-content-lg-between"
-                id="templatemo_main_nav">
-                <div class="flex-fill">
-                    <ul class="nav navbar-nav d-flex justify-content-between mx-lg-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/index') }}">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="about.html">About</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('productIndex') }}">Product</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="contact.html">Contact</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="navbar align-self-center d-flex">
-
-                    <a class="nav-icon d-none d-lg-inline" href="#" data-bs-toggle="modal"
-                        data-bs-target="#templatemo_search">
-                        {{-- <i class="fa fa-fw fa-search text-dark mr-2"></i> --}}
+    <div class="bungkus1">
+        <div class="bungkus-2">
+            <!-- Header -->
+            <nav class="navbar navbar-expand-lg navbar-light shadow">
+                <div class="container d-flex justify-content-between align-items-center">
+        
+                    <a class="navbar-brand text-danger logo h1 align-self-center" href="{{ url('/index') }}">
+                        Roosen
                     </a>
-                    <a class="nav-icon position-relative text-decoration-none" href="{{ url('/cart') }}">
-                        <i class="fa fa-fw fa-cart-arrow-down text-dark mr-1"></i> 
-                        @auth
-                        @if(auth()->user()->role == 'pembeli') 
-                            ( {{ auth()->user()->cart->count() }} )
-                        @endif
-                        @endauth
-                        {{-- <span class="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-light text-dark">7</span> --}}
-                    </a>
-                    @auth
-                        <div class="dropdown">
-                            <button class="btn btn-outline-success dropdown-toggle" type="button" id="dropdownMenuButton1"
-                                data-bs-toggle="dropdown" aria-expanded="false">
-                                {{ auth()->user()->name }}
-                                <i class="fa fa-fw fa-user text-dark mr-3"></i>
-                            </button>
-                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                <li><a class="dropdown-item" href="{{ url('/home') }}">Home</a></li>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST">
-                                    @csrf
-                                    <li><button class="dropdown-item btn btn-danger" type="submit">Log Out</button></li>
-                                </form>
+        
+                    <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#templatemo_main_nav" aria-controls="navbarSupportedContent" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+        
+                    <div class="align-self-center collapse navbar-collapse flex-fill d-lg-flex justify-content-lg-between"
+                        id="templatemo_main_nav">
+                        <div class="flex-fill">
+                            <ul class="nav navbar-nav d-flex justify-content-between mx-lg-auto">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ url('/index') }}">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="about.html">About</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('productIndex') }}">Product</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="contact.html">Contact</a>
+                                </li>
                             </ul>
                         </div>
-                    @endauth
-                    @guest
-                        <a class="nav-icon position-relative text-decoration-none" href="{{ url('/home') }}">
-                            <i class="fa fa-fw fa-user text-dark mr-3"></i>
-                            {{-- <span class="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-light text-dark">7</span> --}}
-                        </a>
-                    @endguest
-                </div>
-            </div>
-
-        </div>
-    </nav>
-    <!-- Close Header -->
-
-
-
-    @yield('main')
-
-    <!-- Start Footer -->
-    <footer class="bg-dark" id="tempaltemo_footer">
-
-        <div class="w-100 bg-black py-3">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12">
-                        <p class="text-center text-light">
-                            Copyright &copy; 2023 Roosen
+                        <div class="navbar align-self-center d-flex">
+        
+                            <a class="nav-icon d-none d-lg-inline" href="#" data-bs-toggle="modal"
+                                data-bs-target="#templatemo_search">
+                                {{-- <i class="fa fa-fw fa-search text-dark mr-2"></i> --}}
                             </a>
-                        </p>
+                            <a class="nav-icon position-relative text-decoration-none" href="{{ url('/cart') }}">
+                                <i class="fa fa-fw fa-cart-arrow-down text-dark mr-1"></i> 
+                                @auth
+                                @if(auth()->user()->role == 'pembeli') 
+                                    ( {{ auth()->user()->cart->count() }} )
+                                @endif
+                                @endauth
+                                {{-- <span class="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-light text-dark">7</span> --}}
+                            </a>
+                            @auth
+                                <div class="dropdown">
+                                    <button class="btn btn-outline-success dropdown-toggle" type="button" id="dropdownMenuButton1"
+                                        data-bs-toggle="dropdown" aria-expanded="false">
+                                        {{ auth()->user()->name }}
+                                        <i class="fa fa-fw fa-user text-dark mr-3"></i>
+                                    </button>
+                                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                        <li><a class="dropdown-item" href="{{ url('/home') }}">Home</a></li>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                                            @csrf
+                                            <li><button class="dropdown-item btn btn-danger" type="submit">Log Out</button></li>
+                                        </form>
+                                    </ul>
+                                </div>
+                            @endauth
+                            @guest
+                                <a class="nav-icon position-relative text-decoration-none" href="{{ url('/home') }}">
+                                    <i class="fa fa-fw fa-user text-dark mr-3"></i>
+                                    {{-- <span class="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-light text-dark">7</span> --}}
+                                </a>
+                            @endguest
+                        </div>
+                    </div>
+        
+                </div>
+            </nav>
+            <!-- Close Header -->
+        
+        
+        
+            @yield('main')
+        </div>
+    
+        <!-- Start Footer -->
+        <footer class="bg-dark" id="tempaltemo_footer">
+    
+            <div class="w-100 bg-black py-3">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <p class="text-center text-light">
+                                Copyright &copy; 2023 Roosen
+                                </a>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-
-    </footer>
-    <!-- End Footer -->
+    
+        </footer>
+        <!-- End Footer -->
+    </div>
 
     <!-- Start Script -->
     <script src="{{ asset('js/jquery-1.11.0.min.js') }}"></script>
